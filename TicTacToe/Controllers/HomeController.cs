79a9593d4a631b8b2fce792fa33bc1f6d0aea75a
SokhibjonZamonov <sokhibjonzamonov@gmail.com>
2022-11-27 17:31:18 +0500

    Add project files.

---
+++ TicTacToe/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TicTacToe.Models;
+
+namespace TicTacToe.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string xPlayer, string oPlayer)
+        {
+            if (string.IsNullOrWhiteSpace(xPlayer) || string.IsNullOrWhiteSpace(oPlayer))
+            {
+                return RedirectToAction(nameof(LoginPage));
+            }
+
+            ViewBag.Player1 = xPlayer;
+            ViewBag.Player2 = oPlayer;
+
+            return View();
+        }
+
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
